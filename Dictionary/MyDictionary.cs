11f41126bf6cd4a467b45982ee57001e988d07ed
Fatih Taşkıran <fatihtaskran@gmail.com>
2021-01-26 22:00:26 +0300

    Add project files.

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyDictionary<TK, TV>
+    {
+        TK[] key;
+        TV[] value;
+        public MyDictionary()
+        {
+            key = new TK[0];
+            value = new TV[0];
+        }
+        public void Add(TK addedKey, TV addedValue)
+        {
+            TK[] _tempKey = key;
+            key = new TK[key.Length + 1];
+
+            TV[] _tempValue = value;
+            value = new TV[value.Length + 1];
+
+            for (int i = 0; i < _tempKey.Length; i++)
+            {
+                key[i] = _tempKey[i];
+                {
+                    for (int j = 0; j < _tempValue.Length; j++)
+                    {
+                        value[j] = _tempValue[j];
+                    }
+                }
+            }
+
+            key[key.Length - 1] = addedKey;
+            value[value.Length - 1] = addedValue;
+
+        }
+        public void List()
+        {
+            for (int i = 0; i < key.Length; i++)
+            {
+                Console.WriteLine("[" + key[i] + ", " + value[i] + "]");
+            }
+        }
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Dictionary<string, int> letterNumber1 = new Dictionary<string, int>
+            {
+                { "a", 1 },
+                { "b", 2 }
+            };
+                Console.WriteLine(letterNumber1["a"]);
+                Console.WriteLine(letterNumber1["b"]);
+                foreach (var show in letterNumber1)
+                {
+                    Console.WriteLine(show);
+                }
+                Console.WriteLine("Part of no Add");
+                letterNumber1.Add("c", 3);
+                foreach (var show2 in letterNumber1)
+                {
+                    Console.WriteLine(show2);
+                }
+
+                Console.WriteLine("C# own dictionary part till here");
+                Console.WriteLine("********************************");
+                Console.WriteLine("My own dictionary part starts from here");
+                MyDictionary<string, int> letterNumber2 = new MyDictionary<string, int>();
+                letterNumber2.Add("a", 1);
+                letterNumber2.Add("b", 2);
+                letterNumber2.Add("c", 3);
+                letterNumber2.Add("d", 4);
+                letterNumber2.List();
+                Console.WriteLine("own dictionary part ends here");
+
+            }
+        }
+    }
+}
